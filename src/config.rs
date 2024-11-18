8a71888891c0e322330ad6cef45690bf03ea539e
rxanders35 <reidxanders@outlook.com>
2024-11-17 22:07:14 -0500

    rough draft for config functions

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
-use serde_json::to_string;
-use std::{fs, io::BufReader, path::Path};
+use std::{error::Error, fs, path::Path};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
@@ -8,10 +7,21 @@ pub struct Config {
     pub current_user_name: String,
 }
 
-pub fn read(path: &str) -> Result<Config> {}
+pub fn read(path: &str) -> Result<Config, Box<dyn Error>> {
+    if !Path::new(path).exists() {
+        return Err(format!("{} does not exist", path).into());
+    }
+    let file = fs::read_to_string(path)?;
+    let config: Config = serde_json::from_str(&file)?;
+
+    Ok(config)
+}
 
 impl Config {
-    pub fn set_user(current_user_name: &str, path: &str) -> Result<Config> {
-        let save = serde_json::to_string_pretty(user).unwrap();
+    pub fn set_user(&mut self, new_user_name: &str, path: &str) -> Result<(), Box<dyn Error>> {
+        self.current_user_name = new_user_name.to_string();
+        let save = serde_json::to_string_pretty(&self)?;
+        fs::write(path, save)?;
+        Ok(())
     }
 }
